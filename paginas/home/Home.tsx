827176b6119e40f38b0e7174c1ac5cd14842242a
PaolaVic <89232691+PaolaVic@users.noreply.github.com>
2024-08-08 20:55:05 -0300

    Add files via upload

---
+++ paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+             <h1 className="titulo">H O M E</h1>
+             <img src="https://wallpapers.com/images/high/neon-distorted-glitch-aesthetic-trippy-smiley-face-raqadeeczj97mvsg.webp" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
