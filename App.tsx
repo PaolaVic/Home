00309089e94f0f8392644f9e38e7a73c9509179e
PaolaVic <89232691+PaolaVic@users.noreply.github.com>
2024-08-09 19:57:05 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
 import Home from './paginas/home/Home';
 import './App.css';
-
+import React, { useState } from 'react';
 
 function App() {
+  const [valor, setValor] = useState(0);
+
+  function handleClick() {
+    window.open('https://www.youtube.com/watch?v=XoYu7K6Ywkg');
+    setValor(valor + 1);
+  }
   return (
-   <Home />
+     <Home  title="Bem-Vindo(a)"
+            description="That’s what you get when you let your heart win."/>
+    //<div>
+      //<p>Escute essa música: {valor}</p>
+     // <button onClick={handleClick}>Ouvir</button>
+   // </div>
   );
 }
 
--- paginas/home/Home.tsx
+++ paginas/home/Home.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Home.css';
 
-function Home(){
+interface minhaProps {
+    title: string;
+    description: string;
+}
+
+function Home(props:minhaProps){
     return (
         <>
              <h1 className="titulo">H O M E</h1>
              <img src="https://wallpapers.com/images/high/neon-distorted-glitch-aesthetic-trippy-smiley-face-raqadeeczj97mvsg.webp" alt="Imagem Tela Inicial" className="img"/>
+             <h2>{props.title}</h2>
+             <p>{props.description}</p>
         </>
     );
 }
